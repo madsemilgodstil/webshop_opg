36ce5a2ef1d9ab0d9fcf4a62459c36b1a72a1387
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-22 10:47:15 +0100

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,106 +1,78 @@
-'use client'
-import Image from 'next/image'
-import Link from 'next/link'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 
-import { useSearchParams } from 'next/navigation'
-import { useCallback } from 'react'
+import { useSearchParams } from "next/navigation";
+import { useCallback } from "react";
 
-export default function Home () {
+export default function Home() {
   // arrays for sizes and colors
-  const sizes = ['sx', 's', 'm', 'l', 'xl']
-  const colors = ['White', 'Black', 'Grey']
-  const searchParams = useSearchParams()
+  const sizes = ["sx", "s", "m", "l", "xl"];
+  const colors = ["White", "Black", "Grey"];
+  const searchParams = useSearchParams();
 
   // console.log(searchParams);
 
   // Her bruger vi useCallback for at oprette en funktion der kan opdatere query params
   const createQueryString = useCallback(
     (name, value) => {
-      const params = new URLSearchParams(searchParams)
-      params.set(name, value)
+      const params = new URLSearchParams(searchParams);
+      params.set(name, value);
 
-      return params.toString()
+      return params.toString();
     },
     [searchParams]
-  )
+  );
 
   return (
-    <main className='flex-1'>
-      <section className='mx-auto grid max-w-7xl p-8'>
+    <main className="flex-1">
+      <section className="mx-auto grid max-w-7xl p-8">
         {/* vi bruger form for at skabe en formular da det er en "formular" man skal udfylde når man vælger størrelse og farve */}
-        <form className='grid gap-2 sm:grid-cols-2 lg:grid-cols-8'>
-          <div className='md:col-span-1 lg:col-span-5'>
-            <div className='aspect-square overflow-hidden bg-neutral-50'>
-              <Image
-                alt='T-shirt'
-                width={500}
-                height={500}
-                className='h-full w-full object-contain object-center'
-                src='/tshirt.png'
-                priority
-              />
+        <form className="grid gap-2 sm:grid-cols-2 lg:grid-cols-8">
+          <div className="md:col-span-1 lg:col-span-5">
+            <div className="aspect-square overflow-hidden bg-neutral-50">
+              <Image alt="T-shirt" width={500} height={500} className="h-full w-full object-contain object-center" src="/tshirt.png" priority />
             </div>
           </div>
-          <div className='flex flex-col pt-6 sm:col-span-1 sm:px-6 sm:pt-0 lg:col-span-3 lg:pt-16'>
+          <div className="flex flex-col pt-6 sm:col-span-1 sm:px-6 sm:pt-0 lg:col-span-3 lg:pt-16">
             <div>
-              <h1 className='mb-4 flex-auto text-3xl font-medium tracking-tight text-neutral-900'>
-                Bare en t-shirt
-              </h1>
-              <p className='mb-8 text-sm'>kr. 90.00</p>
-              <fieldset className='my-4'>
-                <legend className='sr-only'>Sizes</legend>
-                <div className='flex flex-wrap gap-3'>
+              <h1 className="mb-4 flex-auto text-3xl font-medium tracking-tight text-neutral-900">Bare en t-shirt</h1>
+              <p className="mb-8 text-sm">kr. 90.00</p>
+              <fieldset className="my-4">
+                <legend className="sr-only">Sizes</legend>
+                <div className="flex flex-wrap gap-3">
                   {sizes.map((size, index) => {
                     return (
-                      <Link
-                        key={index}
-                        className={`${
-                          searchParams.get('size') == size &&
-                          'border-neutral-100'
-                        } border-neutral-200 text-neutral-900 hover:bg-neutral-100 relative flex min-w-[5ch] items-center justify-center rounded border p-3 text-center text-sm font-semibold`}
-                        href={`?${createQueryString('size', size)}`}
-                      >
+                      <Link key={index} className={`${searchParams.get("size") == size && "border-neutral-100"} border-neutral-200 text-neutral-900 hover:bg-neutral-100 relative flex min-w-[5ch] items-center justify-center rounded border p-3 text-center text-sm font-semibold`} href={`?${createQueryString("size", size)}`}>
                         {size}
                       </Link>
-                    )
+                    );
                   })}
                 </div>
               </fieldset>
-              <fieldset className='my-4'>
-                <legend className='sr-only'>Colors</legend>
-                <div className='flex flex-wrap gap-3'>
+              <fieldset className="my-4">
+                <legend className="sr-only">Colors</legend>
+                <div className="flex flex-wrap gap-3">
                   {colors.map((color, index) => {
                     return (
-                      <Link
-                        key={index}
-                        className={`${
-                          searchParams.get('color') == color &&
-                          'border-neutral-100'
-                        }border-neutral-200 text-neutral-900 hover:bg-neutral-100 relative flex min-w-[5ch] items-center justify-center rounded border p-3 text-center text-sm font-semibold`}
-                        href={`?${createQueryString('color', color)}`}
-                      >
+                      <Link key={index} className={`${searchParams.get("color") == color && "border-neutral-100"}border-neutral-200 text-neutral-900 hover:bg-neutral-100 relative flex min-w-[5ch] items-center justify-center rounded border p-3 text-center text-sm font-semibold`} href={`?${createQueryString("color", color)}`}>
                         {color}
                       </Link>
-                    )
+                    );
                   })}
                 </div>
               </fieldset>
-              <div className='mt-8'>
+              <div className="mt-8">
                 <Link href={`/payment?${searchParams}`}>
-                  <button
-                    type='submit'
-                    className='h-12 items-center rounded-md bg-neutral-900 px-6 py-3 text-base font-medium leading-6 text-white shadow hover:bg-neutral-800'
-                  >
+                  <button type="submit" className="h-12 items-center rounded-md bg-neutral-900 px-6 py-3 text-base font-medium leading-6 text-white shadow hover:bg-neutral-800">
                     <span>Add to cart</span>
                   </button>
                 </Link>
               </div>
-              <div className='mt-8 space-y-6 text-sm text-neutral-500'>
+              <div className="mt-8 space-y-6 text-sm text-neutral-500">
                 <div>
                   <p>
-                    <b>Step into summer with the right balance.</b>&nbsp;Every
-                    time your head goes down, you see these beauties, and your
-                    mood bounces right back up.
+                    <b>Step into summer with the right balance.</b>&nbsp;Every time your head goes down, you see these beauties, and your mood bounces right back up.
                   </p>
                 </div>
               </div>
@@ -109,5 +81,5 @@ export default function Home () {
         </form>
       </section>
     </main>
-  )
+  );
 }
--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 //   Det her er noget helt nyt man kan bruge, men det ikke helt klart endnu
 // Det gør at komponentet ikke er asynkront og syntaksen er lidt anderledes
@@ -10,63 +10,45 @@ const page = async ({ searchParams }) => {
   //   Det her er noget helt nyt man kan bruge, men det ikke helt klart endnu
   //   const { color, size } = use(searchParams);
 
-  const { color, size } = await searchParams
+  const { color, size } = await searchParams;
 
   return (
-    <main className='flex-1'>
-      <section className='mx-auto max-w-7xl p-8'>
-        <h1 className='mt-8 text-3xl font-bold text-neutral-900'>
-          Your Shopping Cart
-        </h1>
-        <form className='mt-12'>
-          <ul
-            role='list'
-            className='divide-y divide-neutral-200 border-b border-t border-neutral-200'
-          >
-            <li className='flex py-4'>
-              <div className='aspect-square h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border bg-neutral-50 sm:h-32 sm:w-32'>
-                <Image
-                  alt='T-shirt'
-                  width={500}
-                  height={500}
-                  className='h-full w-full object-contain object-center'
-                  src='/tshirt.png'
-                  priority
-                />
+    <main className="flex-1">
+      <section className="mx-auto max-w-7xl p-8">
+        <h1 className="mt-8 text-3xl font-bold text-neutral-900">Your Shopping Cart</h1>
+        <form className="mt-12">
+          <ul role="list" className="divide-y divide-neutral-200 border-b border-t border-neutral-200">
+            <li className="flex py-4">
+              <div className="aspect-square h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border bg-neutral-50 sm:h-32 sm:w-32">
+                <Image alt="T-shirt" width={500} height={500} className="h-full w-full object-contain object-center" src="/tshirt.png" priority />
               </div>
-              <div className='relative flex flex-1 flex-col justify-between p-4 py-2'>
-                <div className='flex justify-between justify-items-start gap-4'>
+              <div className="relative flex flex-1 flex-col justify-between p-4 py-2">
+                <div className="flex justify-between justify-items-start gap-4">
                   <div>
-                    <h2 className='font-medium text-neutral-700'>Size:</h2>
+                    <h2 className="font-medium text-neutral-700">Size:</h2>
                     {/* i stedet for get bruger vi dot notation */}
                     {/* <p className="text-sm text-neutral-500">{queryParams.size}</p> */}
 
                     {/* men vi bruger bare size fordi vi har defineret size i vores const queryParams */}
-                    <p className='text-sm text-neutral-500'>{size}</p>
-                    <h2 className='mt-1 font-medium text-neutral-700'>
-                      Color:
-                    </h2>
-                    <p className='text-sm text-neutral-500'>{color}</p>
+                    <p className="text-sm text-neutral-500">{size}</p>
+                    <h2 className="mt-1 font-medium text-neutral-700">Color:</h2>
+                    <p className="text-sm text-neutral-500">{color}</p>
                   </div>
-                  <p className='text-right font-semibold text-neutral-900'>
-                    kr. 90.00
-                  </p>
+                  <p className="text-right font-semibold text-neutral-900">kr. 90.00</p>
                 </div>
-                <div className='flex justify-between'></div>
+                <div className="flex justify-between"></div>
               </div>
             </li>
           </ul>
-          <div className='mt-12'>
-            <div className='mt-10 text-center'>
-              <button className='rounded border border-transparent bg-neutral-900 px-6 py-3 text-center font-medium text-neutral-50 hover:bg-neutral-800 w-full'>
-                Checkout
-              </button>
+          <div className="mt-12">
+            <div className="mt-10 text-center">
+              <button className="rounded border border-transparent bg-neutral-900 px-6 py-3 text-center font-medium text-neutral-50 hover:bg-neutral-800 w-full">Checkout</button>
             </div>
           </div>
         </form>
       </section>
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
